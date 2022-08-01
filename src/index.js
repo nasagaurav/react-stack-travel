c9f5d0762f763994d5e32688b461a573cd1e3d67
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-01 07:46:29 +0530

    steup redux in index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
 import App from './App';
-
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducer';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <Provider store={createStore(reducer)}>
+      <App />
+    </Provider>
   </StrictMode>
 );
