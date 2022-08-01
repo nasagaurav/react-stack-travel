a71b61078ff62eb3b57708d0ca8636c0c75350b4
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-01 07:48:41 +0530

    Admin.js ceated and editted with redux import

---
+++ src/Admin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+function Admin()
+{ 
+  const state=useSelector(s=s)
+  const dispatch=useDispatch()
+  return <div>
+      <h1>Admin</h1>
+    </div>
+}
+export default Admin
