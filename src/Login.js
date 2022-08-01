d9aeb8be8a0eb52f466e388f47955d4806226502
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-01 07:49:43 +0530

    Login.js

---
+++ src/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+function Login() {
+  const state = useSelector((s = s));
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h1>Login</h1>
+    </div>
+  );
+}
+export default Login;
