e6b0f23be4a9c27cdb23c50c152dd97b4bc7b7ae
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-01 07:55:28 +0530

    added routing ad quick fix to = to => in admin,user,login,signup,home.js

--- src/Admin.js
+++ src/Admin.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 function Admin() {
-  const state = useSelector((s = s));
+  const state = useSelector((s => s));
   const dispatch = useDispatch();
   return (
     <div>
--- src/Signup.js
+++ src/Signup.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 function Signup() {
-  const state = useSelector((s = s));
+  const state = useSelector((s => s));
   const dispatch = useDispatch();
   return (
     <div>
