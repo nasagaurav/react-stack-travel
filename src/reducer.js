5030510b68d4456785ab4f94cbfcf396d9d763a1
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-01 07:57:21 +0530

    added reducer intialstate

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,17 @@
-function reducer() {}
+const intialState = {
+  users: [],
+  bookings: [],
+  flights: [],
+  role: '',
+  loggedin: false,
+  user: null,
+};
+
+function reducer(state = intialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
 
 export default reducer;
